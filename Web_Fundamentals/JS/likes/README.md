7a68b700d1dbc46b7675409471f9d06d1fda22fd
Mateo <mtcadillac4@gmail.com>
2023-03-30 16:15:38 +0200

    created a like button

---
+++ Web_Fundamentals/JS/likes/README.md
@@ -0,0 +1,3 @@
+#Dojo Assigments
+
+Created a page that incrases likes everytime you click on like button.
---
+++ Web_Fundamentals/JS/likes/script.js
@@ -0,0 +1,21 @@
+
+var count1 = 9;
+function liked0() {
+    count1++;
+    document.getElementById("number0").innerHTML = count1;
+}
+
+
+var count2 = 12;
+function liked1() {
+
+    count2++;
+    document.getElementById("number1").innerHTML= count2;
+}
+
+
+var count3 = 9;
+function liked2() {
+    count3++;
+    document.getElementById("number2").innerHTML = count3;
+}
